1448b80f612209d00894aa7c44cd5e9d2dd75548
pradiptaagus <igedeaguspradipta@gmail.com>
2018-06-01 17:50:39 +0800

    add engine beli listrik.py and insert Todo

---
+++ Engine beli listrik.py
@@ -0,0 +1,90 @@
+# Engine beli listrik
+# Dibuat tanggal 1 Juni 2018 17:40
+# Ingat hari ini hari kesaktian pancasila
+
+# ============================================
+# Persiapan
+# ============================================
+
+# Todo import library
+# Todo membuat function
+# pisahkan masing-masing proses dengan membuat function
+
+
+# Todo membuat koneksi
+
+
+# Todo membuat cursor
+
+
+# Todo select semua data dari tabel transaksi PLN dan Indomaret
+
+
+# Todo Menghitung banyak data dari tabel transaksi PLN dan Indomaret
+
+
+# ============================================
+# Mulai proses 
+# ============================================
+# Cek tabel transaksi PLN
+# ============================================
+
+# Todo cek update dari tabel transaksi PLN
+
+
+# Todo cek insert dari tabel transaksi PLN
+
+
+# Todo cek delete dari tabel transaksi PLN
+
+
+# ============================================
+# Mulai proses 
+# ============================================
+# Cek tabel transaksi Indomaret
+# ============================================
+
+# Todo cek update dari tabel transaksi PLN
+
+
+# Todo cek insert dari tabel transaksi PLN
+
+
+# Todo cek delete dari tabel transaksi PLN
+
+
+# ============================================
+# Mulai proses 
+# ============================================
+# Cek file JSON PLN
+# ============================================
+
+# Todo cek update dari tabel transaksi PLN
+
+
+# Todo cek insert dari tabel transaksi PLN
+
+
+# Todo cek delete dari tabel transaksi PLN
+
+
+# ============================================
+# Mulai proses 
+# ============================================
+# Cek file JSON Indomaret
+# ============================================
+
+# Todo cek update dari tabel transaksi PLN
+
+
+# Todo cek insert dari tabel transaksi PLN
+
+
+# Todo cek delete dari tabel transaksi PLN
+
+
+# Todo melakukan infinity loop
+while True:
+	# Todo menentukan delay dari engine dengan meminta input dari user
+	delay = int(input("Masukkan waktu delay: "))
+	# Todo memanggil fungsi main
